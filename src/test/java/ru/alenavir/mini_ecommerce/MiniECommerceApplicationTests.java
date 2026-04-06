5b8f6a5b369f28eb703f79b14ea12972711b3a4f
alenavir <wandererNavi@gmail.com>
2026-04-06 12:30:52 +0300

    add ci.yml

--- src/test/java/ru/alenavir/mini_ecommerce/MiniECommerceApplicationTests.java
+++ src/test/java/ru/alenavir/mini_ecommerce/MiniECommerceApplicationTests.java
@@ -3,7 +3,6 @@ package ru.alenavir.mini_ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MiniECommerceApplicationTests {
 
     @Test
